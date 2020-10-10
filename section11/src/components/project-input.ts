bb2ed329a869b92b2511e6d29db12af893badb2e
MrRobot420 <max.karlchen@web.de>
2020-10-10 11:18:40 +0200

    add section11

---
+++ section11/src/components/project-input.ts
@@ -0,0 +1,61 @@
+import { Component }  from './base-components.js'
+import { autobind } from "../decorators/autobind.js"
+import { projectState } from "../state/project-state.js"
+import * as Validation from "../utils/validation.js"
+
+     // ProjectInput Class
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement
+    descriptionElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
+
+    constructor() {
+        super('project-input', 'app', true, 'user-input')
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionElement = this.element.querySelector('#description') as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+        this.configure()
+    }
+
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler)
+    }
+
+    renderContent() { }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value
+        const enteredDescription = this.descriptionElement.value
+        const enteredPeople = this.peopleInputElement.value
+
+        const titleValidatable: Validation.Validatable = { value: enteredTitle, required: true }
+        const descValidatable: Validation.Validatable = { value: enteredDescription, required: true, minLength: 5 }
+        const peopleValidatable: Validation.Validatable = { value: +enteredPeople, required: true, min: 1, max: 5 }
+
+        if (!Validation.validate(titleValidatable) || !Validation.validate(descValidatable) || !Validation.validate(peopleValidatable)) {
+            alert('Invalid input, please try again!')
+            return
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        const userInput = this.gatherUserInput()
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput
+            projectState.addProject(...userInput)
+            console.log(title, desc, people)
+            this.clearInputs()
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = ''
+        this.descriptionElement.value = ''
+        this.peopleInputElement.value = ''
+    }
+}
---
+++ section11/src/decorators/autobind.ts
@@ -0,0 +1,17 @@
+// autobind decorator
+export function autobind(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjustedDescriptor
+}
