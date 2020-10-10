ac4bf4c04ecbd123cf7f405983b5d64508bb69da
MrRobot420 <max.karlchen@web.de>
2020-10-10 17:56:48 +0200

    add full functionality of maps

--- section13/src/app.ts
+++ section13/src/app.ts
@@ -5,9 +5,11 @@ const form = document.querySelector('form')!
 const addressInput = document.getElementById('address')! as HTMLInputElement
 
 type GoogleGeocodingResponse = { 
-    results: { geometry: { location: { lat: number, long: number } } }[]
+    results: { geometry: { location: { lat: number, lng: number } } }[]
     status: 'OK' | 'ZERO_RESULTS'
 }
+
+// declare var google: any
 
 
 function searchAddressHandler(event: Event) {
@@ -16,11 +18,19 @@ function searchAddressHandler(event: Event) {
 
     const API_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`
 
-    axios.get<GoogleGeocodingResponse>(API_URL).then(response => {
+    axios.get<GoogleGeocodingResponse>(API_URL)
+    .then(response => {
         if (response.data.status !== 'OK') {
             throw new Error('Could not fetch location!')
         }
+        console.log(response.data)
         const coordinates = response.data.results[0].geometry.location
+        const map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+            center: coordinates,
+            zoom: 16,
+          })
+        new google.maps.Marker({ position: coordinates, map: map })
+  
     }).catch(err => {
         console.log(err)
     })
