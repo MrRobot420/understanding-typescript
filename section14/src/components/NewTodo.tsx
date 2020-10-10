293c97d660b540e8d9ac60e493fe076a40b37d1d
MrRobot420 <max.karlchen@web.de>
2020-10-10 20:38:31 +0200

    add state management

--- section14/src/components/NewTodo.tsx
+++ section14/src/components/NewTodo.tsx
@@ -1,13 +1,16 @@
 import React, { useRef } from 'react';
 
-const NewTodo: React.FC = () => {
+interface NewTodoProps {
+    onAddTodo: (todoText: string) => void
+}
+
+const NewTodo: React.FC<NewTodoProps> = props => {
     const textinputRef = useRef<HTMLInputElement>(null)
 
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault()
         const enteredText = textinputRef.current!.value
-        console.log(enteredText)
-        
+        props.onAddTodo(enteredText)
     }
 
     return <form onSubmit={todoSubmitHandler}>
