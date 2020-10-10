88bebf186f1a9bd227f531321da1bf3132983b47
MrRobot420 <max.karlchen@web.de>
2020-10-10 17:38:21 +0200

    add axios | add config.js with API key | add google maps geocode api request

--- section13/src/app.ts
+++ section13/src/app.ts
@@ -1,11 +1,29 @@
+import { GOOGLE_API_KEY } from "../config"
+import axios from 'axios'
+
 const form = document.querySelector('form')!
 const addressInput = document.getElementById('address')! as HTMLInputElement
 
-function searchAddressHandler (event: Event) {
+type GoogleGeocodingResponse = { 
+    results: { geometry: { location: { lat: number, long: number } } }[]
+    status: 'OK' | 'ZERO_RESULTS'
+}
+
+
+function searchAddressHandler(event: Event) {
     event.preventDefault()
     const enteredAddress = addressInput.value
 
-    // send this to google api
+    const API_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`
+
+    axios.get<GoogleGeocodingResponse>(API_URL).then(response => {
+        if (response.data.status !== 'OK') {
+            throw new Error('Could not fetch location!')
+        }
+        const coordinates = response.data.results[0].geometry.location
+    }).catch(err => {
+        console.log(err)
+    })
 }
 
 form.addEventListener('submit', searchAddressHandler)
