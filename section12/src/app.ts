04a548a284869888361e59b703721ff0ba1e6c50
MrRobot420 <max.karlchen@web.de>
2020-10-10 16:10:39 +0200

    normal transformation

--- section12/src/app.ts
+++ section12/src/app.ts
@@ -1,4 +1,17 @@
 import { Product } from "./product.model"
 
-const p1 = new Product('A Book', 12.99)
-console.log(p1.getInformation())
+// const p1 = new Product('A Book', 12.99)
+
+const products = [
+    { title: 'A Book', price: 12.99 }, 
+    { title: 'A Couch', price: 129.99 }
+]
+
+const loadedProducts = products.map(prod => {
+    return new Product(prod.title, prod.price)
+})
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation())
+}
+
