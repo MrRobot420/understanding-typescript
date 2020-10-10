7c7658fc7d60a7a4baffd15b05931b3426e7ecbe
MrRobot420 <max.karlchen@web.de>
2020-10-10 16:35:34 +0200

    add validation example

--- section12/src/app.ts
+++ section12/src/app.ts
@@ -1,12 +1,23 @@
 import 'reflect-metadata'
 import { plainToClass } from 'class-transformer'
 import { Product } from "./product.model"
+import { validate } from "class-validator";
 
 
 const products = [
     { title: 'A Book', price: 12.99 }, 
     { title: 'A Couch', price: 129.99 }
 ]
+
+const newProd = new Product('', -6.99)
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log('VALIDATION ERRORS!')
+        console.log(errors)
+    } else {
+        console.log(newProd.getInformation())
+    }
+})
 
 // const p1 = new Product('A Book', 12.99)
 
