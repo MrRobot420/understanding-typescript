c8f5bf19e9304740c0442ff384caddf237fb863d
MrRobot420 <max.karlchen@web.de>
2020-10-11 11:11:28 +0200

    setup routes

--- section15/dist/app.js
+++ section15/dist/app.js
@@ -4,6 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./routes/todos"));
 const app = express_1.default();
+app.use('/todos', todos_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
