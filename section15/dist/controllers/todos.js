0e4a3cf18fcf9e1a19e29d18a2f154682c5d7245
MrRobot420 <max.karlchen@web.de>
2020-10-11 11:54:08 +0200

    add the rest of the CRUD ops

--- section15/dist/controllers/todos.js
+++ section15/dist/controllers/todos.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 exports.createTodo = (req, res, next) => {
@@ -8,4 +8,26 @@ exports.createTodo = (req, res, next) => {
     const newTodo = new todo_1.Todo(Math.random().toString(), text);
     TODOS.push(newTodo);
     res.status(201).json({ message: 'Created the todo', createdTodo: newTodo });
+};
+exports.getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('Could not find todo!');
+    }
+    TODOS[todoIndex] = new todo_1.Todo(TODOS[todoIndex].id, updatedText);
+    res.status(200).json({ message: `Updated todo with ID: ${todoId}`, updatedTodo: TODOS[todoIndex] });
+};
+exports.deleteTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('Could not find todo!');
+    }
+    TODOS.splice(todoIndex, 1);
+    res.status(202).json({ message: 'Deleted Todo!', currentTodos: TODOS });
 };
